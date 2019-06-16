4f7dcc2020300c624482d1f206f88a750561d45f
PratikVagyani <pratik.vagyani2nov@gmail.com>
2019-06-16 05:39:21 -0400

    Not required

--- scripts.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-  var sectorCount = 16;
-  $('button').on('click', function(e){
-    var randomSector = Math.floor(Math.random() * sectorCount) + 1;
-    $('.image').attr('class', 'image sector-' + randomSector);
-  }).trigger('click');
-});
